1835bf14ef4b0ab58dc26c70ab3c0cf4a556bae0
Rishabh Sood <rishabh.sood@flipkart.com>
2016-02-25 22:19:15 +0530

    UI change

--- src/main/resources/assets/js/Merchant.js
+++ src/main/resources/assets/js/Merchant.js
@@ -36,7 +36,7 @@ function addRow(orderId, status, time, price, name, phone) {
         background = "red";
     }
     var table = $('#orderlist');
-    table.append('<tr onclick=\"onRowClick(\''+orderId+'\')\" style=\"background-color:'+background+'\"><td <div class=\"col-sm-2\">' +
+    table.append('<tr onclick=\"onRowClick(\''+orderId+'\',\''+status+'\')\" style=\"background-color:'+background+'\"><td <div class=\"col-sm-2\">' +
     '<label class="control-label">' + orderId + '</label>' +
     '</div>  </td> <td <div class=\"col-sm-2\">' +
     '<label class="control-label">' + status + '</label>' +
@@ -51,38 +51,73 @@ function addRow(orderId, status, time, price, name, phone) {
         '</div>  </td></tr>')
 }
 
-function onRowClick(orderId) {
+function onRowClick(orderId, status) {
     $.ajax({
         url: "api/foodmania/customer/getOrderItem/"+orderId,
         type: "GET",
         contentType: "application/json",
         async: false,
         success: function (data) {
+            document.getElementById("orderId").innerHTML = orderId;
+            var table = $('#itemlist');
+            table.empty();
+            table.append('<thead>' +
+                '<tr>'+
+                '<th>Item Name</th>'+
+            '<th>Price</th>'+
+            '<th>Count</th>'+
+            '</tr>'+
+            '</thead>');
             var order = data.itemForOrderEntities;
             console.log(data);
             for (var i = 0; i < order.length; i++) {
-                addItemRow(order[i].itemid, order[i].itemdesc, order[i].price, order[i].count) ;
+                addItemRow(order[i].itemdesc, order[i].price, order[i].count);
             }
+            $("#statuslist").html(status+"<span class=\"caret\"></span>")
             $('#itemModal').modal();
         },
         error: function () {
-            alert("Error in finding Job List!!");
+            alert("Error in finding Order List!!");
         },
         complete: function () {
         }
     })
 }
 
-function addItemRow(itemid, itemdesc, price, count) {
+function addItemRow(itemdesc, price, count) {
     var table = $('#itemlist')
-    document.getElementById("orderId").innerText= orderId;
     table.append('<tr><td <div class=\"col-sm-2\">' +
-        '<label class="control-label">' + itemid + '</label>' +
-        '</div>  </td> <td <div class=\"col-sm-2\">' +
         '<label class="control-label">' + itemdesc + '</label>' +
         '</div>  </td> <td <div class=\"col-sm-2\">' +
         '<label class="control-label">' + price + '</label>' +
         '</div>  </td><td <div class=\"col-sm-2\">' +
         '<label class="control-label">' + count + '</label>' +
         '</div>  </td></tr>');
+}
+
+//Dropdown box
+$(document).on('click', '#statuslistdropdown li a', function() {
+    var jobName = $(this).text();
+    $("#statuslist").html(jobName+"<span class=\"caret\"></span>")
+});
+
+function updateOrderStatus() {
+    var orderId = $("#orderId").html();//document.getElementById("orderId").textContent
+    var status = document.getElementById("statuslist").firstChild.data.trim();
+    var json = "{\"status\":\""+status+"\"}"
+    $.ajax({
+        url: "/api/foodmania/customer/updateOrder/"+orderId,
+        type: "POST",
+        contentType: "application/json",
+        data: json,
+        success: function (data) {
+            alert("status updated Successfully");
+            $('#itemModal').modal('hide');
+        },
+        error: function () {
+            alert("Error in updating status!!");
+        },
+        complete: function () {
+        }
+})
 }
